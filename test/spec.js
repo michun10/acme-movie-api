e5673685c7f4049a0b3f3702f46fcf8f80cd5c3e
Michelle Un <michelleun@gmail.com>
2023-01-19 20:00:05 -0500

    fixing test

--- test/spec.js
+++ test/spec.js
@@ -3,6 +3,6 @@ const { add } = require("../utilities");
 
 describe("add function", () => {
   it("adds two numbers", () => {
-    expect(add(2, 3).to.equal(5));
+    expect(add(2, 3)).to.equal(5);
   });
 });
