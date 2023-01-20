11c51a60b549cd39c8c87c53ab30c77449317d53
Michelle Un <michelleun@gmail.com>
2023-01-19 20:22:46 -0500

    add seq tsts

--- test/spec.js
+++ test/spec.js
@@ -1,6 +1,11 @@
 const { expect } = require("chai");
 const { add } = require("../utilities");
 const server = require("supertest")(require("../app"));
+const {
+  syncAndSeed,
+  models: { Actor, Movie, Role },
+} = require("../db");
+const sequelize = require("sequelize");
 
 describe("add function", () => {
   it("adds two numbers", () => {
@@ -12,5 +17,18 @@ describe("GET /", () => {
   it("returns 200 OK", async () => {
     res = await server.get("/api/movies");
     expect(res.status).to.equal(200);
+  });
+});
+
+describe("models", () => {
+  beforeEach(async () => {
+    await syncAndSeed();
+  });
+
+  describe("movies", () => {
+    it("has 4 movies", async () => {
+      const movies = await Movie.findAll();
+      expect(movies.length).to.equal(4);
+    });
   });
 });
