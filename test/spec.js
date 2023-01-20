5da3d958a15675558678a7010dbbb84829e91846
Michelle Un <michelleun@gmail.com>
2023-01-19 19:51:52 -0500

    adding test

---
+++ test/spec.js
@@ -0,0 +1,8 @@
+const { expect } = require("chai");
+const { add } = require("../utilities");
+
+describe("add function", () => {
+  it("adds two numbers", () => {
+    expect(add(2, 3).to.equal(5));
+  });
+});
