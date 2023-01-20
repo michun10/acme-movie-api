939908c069a96e1290e1779a17ff5da1a725c33b
Michelle Un <michelleun@gmail.com>
2023-01-19 20:06:02 -0500

    adding route

---
+++ app.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const app = express();
+
+app.get("/api/movies", async (req, res, next) => {
+  res.sendStatus(200);
+});
+
+module.exports = { app };
--- test/spec.js
+++ test/spec.js
@@ -1,8 +1,16 @@
 const { expect } = require("chai");
 const { add } = require("../utilities");
+const server = require("supertest")(require("../app"));
 
 describe("add function", () => {
   it("adds two numbers", () => {
     expect(add(2, 3)).to.equal(5);
+  });
+});
+
+describe("GET /", () => {
+  it("returns 200 OK", async () => {
+    res = await server.get("/api/movies");
+    expect(res.status).to.equal(200);
   });
 });
